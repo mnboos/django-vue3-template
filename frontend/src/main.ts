b8f402cd72b546cbe1cd546998d829d16a12e59a
Martin Boos <mboos@outlook.com>
2022-02-21 10:02:33 +0100

    Add: quasar UI framework

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,12 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { Quasar } from "quasar";
+
+// Import icon libraries
+import "@quasar/extras/material-icons/material-icons.css";
+
+// Import Quasar css
+import "quasar/src/css/index.sass";
 
 import App from "./App.vue";
 import router from "./router";
@@ -8,5 +15,21 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+app.use(Quasar, {
+  plugins: {}, // import Quasar plugins and add here
+  /*
+  config: {
+    brand: {
+      // primary: '#e46262',
+      // ... or all other brand colors
+    },
+    notify: {...}, // default set of options for Notify Quasar plugin
+    loading: {...}, // default set of options for Loading Quasar plugin
+    loadingBar: { ... }, // settings for LoadingBar Quasar plugin
+    // ..and many more (check Installation card on each Quasar component/directive/plugin)
+  }
+  */
+});
 
 app.mount("#app");
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,10 +2,18 @@ import { fileURLToPath, URL } from "url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: { transformAssetUrls },
+    }),
+    quasar({
+      sassVariables: "src/quasar-variables.sass",
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
