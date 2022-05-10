4a7f3ef2f210e2f1b8a5e3bb52858b9b21200af2
Martin Boos <mboos@outlook.com>
2022-05-10 07:25:27 +0200

    Format: vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,17 +6,17 @@ import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue({
-      template: { transformAssetUrls },
-    }),
-    quasar({
-      sassVariables: "src/quasar-variables.sass",
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [
+        vue({
+            template: { transformAssetUrls },
+        }),
+        quasar({
+            sassVariables: "src/quasar-variables.sass",
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-  },
 });
